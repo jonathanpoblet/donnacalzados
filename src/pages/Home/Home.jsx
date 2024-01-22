a10b98078a62ec0f5cdda2ba60e2e5baa2417001
unknown <jonathanpoblet228@gmail.com>
2024-01-22 15:44:47 -0300

    Banner Spiner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import HomeCategories from '../../components/HomeCategories/HomeCategories';
 import HomeContact from '../../components/HomeContact/HomeContact';
@@ -12,13 +12,30 @@ import './home.css';
 
 export default function Home() {
   const navigate = useNavigate();
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   useEffect(() => {
     AOS.init();
   }, []);
+
+  const handleImageLoad = () => {
+    // La imagen se ha cargado completamente, ocultar el spinner
+    setImageLoaded(true);
+  };
+
   return (
     <main className='home'>
-      {/* <HomeBanner /> */}
-      <img className='home-img' src='../donnacalzados/assets/home/banner.png' alt='banner' data-aos='fade-up' data-aos-offset='100' data-aos-easing='ease-in-sine' data-aos-duration='600' />
+      {!imageLoaded && <Spinner />} {/* Mostrar el spinner mientras la imagen no se haya cargado */}
+      <img
+        className={`home-img ${imageLoaded ? 'loaded' : ''}`}
+        src='../donnacalzados/assets/home/banner.png'
+        alt='banner'
+        data-aos='fade-up'
+        data-aos-offset='100'
+        data-aos-easing='ease-in-sine'
+        data-aos-duration='600'
+        onLoad={handleImageLoad} // Evento onLoad para detectar la carga completa de la imagen
+      />
       <HomeProducts />
       <HomeInfo />
       <HomeCategories />
@@ -26,3 +43,5 @@ export default function Home() {
     </main>
   );
 }
+
+const Spinner = () => <div className='spinner'>Cargando...</div>;
--- src/router/AppRoute.jsx
+++ src/router/AppRoute.jsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { lazy, Suspense } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
 import ScrollToTopOnUrlChange from '../utils/scroll';
@@ -9,43 +9,28 @@ const Woman = lazy(() => import('../pages/Woman/Woman'));
 const Child = lazy(() => import('../pages/Child/Child'));
 const AboutUs = lazy(() => import('../pages/AboutUs/AboutUs'));
 const Detail = lazy(() => import('../pages/Detail/Detail'));
+
 const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+
 const Spinner = lazy(() => import('../components/Spinner/Spinner'));
 
-const AppRoute = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simular una carga de 3 segundos antes de mostrar el spinner
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+export default function AppRoute() {
   return (
     <Suspense fallback={<Spinner />}>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <HashRouter>
-          <ScrollToTopOnUrlChange />
-          <Layout>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/hombre' element={<Man />} />
-              <Route path='/mujer' element={<Woman />} />
-              <Route path='/niño' element={<Child />} />
-              <Route path='/nosotros' element={<AboutUs />} />
-              <Route path='/detalle' element={<Detail />} />
-              <Route path='*' element={<NotFound />} />
-            </Routes>
-          </Layout>
-        </HashRouter>
-      )}
+      <HashRouter>
+        <ScrollToTopOnUrlChange />
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/hombre' element={<Man />} />
+            <Route path='/mujer' element={<Woman />} />
+            <Route path='/niño' element={<Child />} />
+            <Route path='/nosotros' element={<AboutUs />} />
+            <Route path='/detalle' element={<Detail />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
     </Suspense>
   );
-};
-
-export default AppRoute;
+}
