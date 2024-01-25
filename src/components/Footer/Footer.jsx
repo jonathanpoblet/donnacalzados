61b02c8522dc89509e1bb7feae206f5ea2eace00
unknown <jonathanpoblet228@gmail.com>
2024-01-25 16:47:22 -0300

    Footer Finished Full Responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,73 +5,89 @@ import './footer.css';
 import { Link } from 'react-router-dom';
 
 export default function Footer() {
+  const cards = [
+    '../assets/payments/payment1.png',
+    '../assets/payments/payment2.png',
+    '../assets/payments/payment3.png',
+    '../assets/payments/payment4.png',
+    '../assets/payments/payment5.png',
+    '../assets/payments/payment6.png',
+    '../assets/payments/payment7.png',
+    '../assets/payments/payment8.png',
+    '../assets/payments/payment9.png',
+    '../assets/payments/payment10.png',
+    '../assets/payments/payment11.png',
+    '../assets/payments/payment12.png',
+    '../assets/payments/payment13.png',
+  ];
   return (
-    <footer className='page-footer font-small blue pt-4'>
-      <div className='container-fluid text-center text-md-left py-3'>
-        <div className='row'>
-          <div className='col-md-4 mb-md-0 mb-3 footer-container1'>
-            <h5 className='text-uppercase text-white'>NAVEGACIÓN</h5>
-            <ul className='list-unstyled d-flex flex-column justify-content-center'>
-              <li className='d-flex flex-row align-items-center justify-content-center ms-md-3 my-2 text-start'>
-                <Link className='footer-link' to='./'>
-                  Inicio
-                </Link>
-              </li>
-              <li className='d-flex flex-row align-items-center justify-content-center ms-md-3 my-2 text-start'>
-                <Link className='footer-link' to='./hombre'>
-                  Hombre
-                </Link>
-              </li>
-              <li className='d-flex flex-row align-items-center justify-content-center ms-md-3 my-2 text-start'>
-                <Link className='footer-link' to='./mujer'>
-                  Mujer
-                </Link>
-              </li>
-              <li className='d-flex flex-row align-items-center justify-content-center ms-md-3 my-2 text-start'>
-                <Link className='footer-link' to='./niño'>
-                  Niño/a
-                </Link>
-              </li>
-              <li className='d-flex flex-row align-items-center justify-content-center ms-md-3 my-2 text-start'>
-                <Link className='footer-link' to='./informacion'>
-                  Información
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className='col-md-4 mb-md-0 mb-3 footer-container2'>
-            <h5 className='text-uppercase text-start text-white'>CONTACTO</h5>
-            <ul className='list-unstyled'>
-              <li className='d-flex flex-row align-items-center my-4'>
-                <MdLocationOn className='footer-container-info-icon me-2' />
-                <h4 className='footer-container-info-text'>Buenos Aires, Argentina</h4>
-              </li>
-              <li className='d-flex flex-row align-items-center my-4'>
-                <BsFillEnvelopeFill className='footer-container-info-icon me-2' />
-                <h4 className='footer-container-info-text'>donnacalzados@gmail.com</h4>
-              </li>
-              <li className='d-flex flex-row align-items-center my-4'>
-                <BsFillTelephoneFill className='footer-container-info-icon me-2' />
-                <h4 className='footer-container-info-text'>11 5046-0184</h4>
-              </li>
-            </ul>
-          </div>
-          <div className='col-md-4 mb-md-0 mb-3 footer-container3'>
-            <h5 className='text-uppercase text-start text-white'>REDES</h5>
-            <ul className='list-unstyled'>
-              <li className='d-flex flex-row align-items-center my-4'>
-                <BsWhatsapp onClick={() => window.open('https://wa.me/1150460184', '_blank')} className='footer-container-info-icon me-3' />
-                <BsInstagram onClick={() => window.open('https://www.instagram.com/donnacalzadosymoda/', '_blank')} className='footer-container-info-icon me-3' />
-                <BsFacebook onClick={() => window.open('https://www.facebook.com/donnaflores20', '_blank')} className='footer-container-info-icon me-3' />
-              </li>
-            </ul>
-          </div>
+    <footer className='footer'>
+      <div className='footer-container'>
+        <div className='footer-container-div'>
+          <h5 className='footer-container-div-title'>NAVEGACIÓN</h5>
+          <ul className='footer-container-div-list'>
+            <li className='footer-container-div-list-item'>
+              <Link className='footer-container-div-list-item-link' to='./'>
+                Inicio
+              </Link>
+            </li>
+            <li className='footer-container-div-list-item'>
+              <Link className='footer-container-div-list-item-link' to='./productos'>
+                Productos
+              </Link>
+            </li>
+            <li className='footer-container-div-list-item'>
+              <Link className='footer-container-div-list-item-link' to='./nosotros'>
+                Nosotros
+              </Link>
+            </li>
+            <li className='footer-container-div-list-item'>
+              <Link className='footer-container-div-list-item-link' to='./compras-mayoristas'>
+                Compras Mayoristas
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className='footer-container-div'>
+          <h5 className='footer-container-div-title'>MÉTODOS DE PAGO</h5>
+          <ul className='footer-container-div-payment'>
+            {cards.map(c => {
+              return (
+                <li key={c} className='footer-container-div-payment-item'>
+                  <img src={c} alt='Método de Pago' />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className='footer-container-div'>
+          <h5 className='footer-container-div-title'>CONTACTO</h5>
+          <ul className='footer-container-div-list'>
+            <li className='footer-container-div-list-item'>
+              <BsFillTelephoneFill className='footer-container-div-list-item-icon' />
+              <h4 className='footer-container-div-list-item-info'>11 5046-0184</h4>
+            </li>
+            <li className='footer-container-div-list-item'>
+              <MdLocationOn className='footer-container-div-list-item-icon' />
+              <h4 className='footer-container-div-list-item-info'>Buenos Aires, Argentina</h4>
+            </li>
+            <li className='footer-container-div-list-item'>
+              <BsFillEnvelopeFill className='footer-container-div-list-item-icon' />
+              <h4 className='footer-container-div-list-item-info'>donnacalzados@gmail.com</h4>
+            </li>
+          </ul>
         </div>
       </div>
 
-      <div className='footer-copyright text-center py-3 bg-black text-white'>
-        © 2024 Copyright Donna Calzados | Desarrollado por <span onClick={() => window.open('https://www.instagram.com/virtual.designs.ar/', '_blankc')}>Virtual Designs</span>
+      <div className='footer-copyright'>
+        <div className='footer-copyright-container'>
+          <p>© 2024 Copyright Donna Calzados</p>
+          <div>
+            <BsWhatsapp onClick={() => window.open('https://wa.me/1150460184', '_blank')} />
+            <BsInstagram onClick={() => window.open('https://www.instagram.com/donnacalzadosymoda/', '_blank')} />
+            <BsFacebook onClick={() => window.open('https://www.facebook.com/donnaflores20', '_blank')} />
+          </div>
+        </div>
       </div>
     </footer>
   );
